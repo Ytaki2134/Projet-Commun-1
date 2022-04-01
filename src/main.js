354e5b4ff3a2de930238208ef1a83d55dd9fb0e0
RobxSoft <48138370+RobxSoft@users.noreply.github.com>
2022-04-01 10:41:14 +0200

    local storage done

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import Data from "./utilities/dataHandling.js";
 let DataObject = new Data();
 let PlayerData = DataObject.getData();
 
-console.log(PlayerData);
-
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -30,9 +28,18 @@ function create ()
 {
     let pos = 0;
     for (const [key, value] of Object.entries(PlayerData.Currency)) {
-        this.add.text(100*pos, 0, key+' : '+value, { font: '"Press Start 2P"' });
+        let txt = this.add.text(100*pos, 0, key+' : '+value, { font: '"Press Start 2P"' });
         pos += 1;
+
+        DataObject.On(key, function(newValue){
+            txt.setText(key+' : '+newValue);
+        })
     }
+
+    setTimeout(() => {
+        PlayerData.Currency.Gold +=1; //set value
+        DataObject.updateData(); //update
+    }, 2000);
 }
 
 var game = new Phaser.Game(config);
--- src/utilities/dataHandling.js
+++ src/utilities/dataHandling.js
@@ -2,20 +2,23 @@ const baseData = {
     Currency: {
         Gold: 0,
         Crystals: 0,
-    }
+    },
 }
 
 class Data{
     constructor(){
         let playerData = localStorage.getItem('data');
-        if(!playerData){
+        if(!playerData || playerData == null){
             localStorage.setItem('data', JSON.stringify(baseData));
             playerData = localStorage.getItem('data');
         }else{
+            console.log("?");
             playerData = this.fixData(JSON.parse(playerData), baseData);
             localStorage.setItem('data', JSON.stringify(playerData));
         }
-        this.playerData = playerData;
+        this.playerData = {...playerData};
+        this.savedPlayerData = JSON.parse(JSON.stringify(this.playerData)); // DEEP CLONE
+        this.listenValueChanged = {};
     }
 
     fixData(playerData, base){
@@ -28,12 +31,33 @@ class Data{
                 playerData[key] = this.fixData(playerData[key], value);
             }
         }
-        console.log(playerData);
         return playerData 
     }
 
     getData(){
         return this.playerData;
+    }
+
+    On(key, fct){
+        this.listenValueChanged[key] = fct;
+    }
+
+    onChangeValue(original, clone){ //clone should be on an hight version than original
+        for (const [key, value] of Object.entries(clone)) {
+            if(typeof(value)==="object" && value != null){
+                this.onChangeValue(original[key], clone[key]);
+            }else if(original[key] != undefined && original[key] != value){
+                if(this.listenValueChanged[key]){
+                    this.listenValueChanged[key](value);
+                }
+            }
+        }
+    }
+
+    updateData(){
+        this.onChangeValue(this.savedPlayerData, this.playerData);
+        this.savedPlayerData = JSON.parse(JSON.stringify(this.playerData));;
+        localStorage.setItem('data', JSON.stringify(this.savedPlayerData));
     }
 }
 
