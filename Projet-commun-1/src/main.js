acb31fdd8513c8090b48c7e55812ececab611d88
TOM LE_GROS <freezekaz.dement@gmail.com>
2022-04-04 19:07:34 +0200

    tom merge and stuff

--- Projet-commun-1/src/main.js
+++ Projet-commun-1/src/main.js
@@ -1,11 +1,12 @@
 import Data from "./utilities/dataHandling.js";
 import * as apiUtilities from "./utilities/apiUtilities.js";
-
+//hi
 const attractions = await apiUtilities.getAttractions();
 
 let DataObject = new Data();
 let PlayerData = DataObject.getData();
 
+let selectedBuilding = null;
 
 var config = {
     type: Phaser.AUTO,
@@ -19,36 +20,60 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
-
 function preload ()
 {
     //this.load.setBaseURL('http://labs .phaser.io');
     this.load.image('background', 'image/background.png');
-    this.load.image('field', 'image/field.png');
-    this.load.image('built', 'image/built.png');
+    this.load.spritesheet('field', 'image/yow.png', { frameWidth: 222, frameHeight: 121 });
+}
+
+function disableAll()
+{
+    attractions.forEach(obj => (
+        window[obj.name].disableInteractive()
+    ));
+}
+
+function enableAll()
+{
+    attractions.forEach(obj => (
+        window[obj.name].setInteractive()
+    ));
 }
 
 function create ()
 {   
-    //alert(myBuildings[0].name);
-    //console.log(attractions[2].name);
-    var argent = 1500; // simulation de l'argent de l'utilisateur
+    //Event listener
+    let popup = document.getElementById('construction');
+    let closeButton = document.getElementById("close");
+    let build = document.getElementById("build-button");
+    build.addEventListener("click", construire)
+
+    closeButton.addEventListener("click", function(){
+        popup.style.display = 'none';
+    })
+
+    
     this.add.image(900, 485, 'background'); 
 
     attractions.forEach(obj => {
         console.log(obj.name);
-        window[obj.name] = this.add.image(    
+        window[obj.name] = this.add.sprite(    
             obj.pos_x,
             obj.pos_y,
             obj.image);
-        window[obj.name].setInteractive().on('pointerdown', () => checkBuilding(obj,argent));
+        window[obj.name].setInteractive().on('pointerdown', () => checkBuilding(obj));
+        window[obj.name].setFrame(0);
+    
 
     });
+
     if (PlayerData.UnlockedBuildings != [])
     {
         PlayerData.UnlockedBuildings.forEach(element => {
@@ -56,6 +81,7 @@ function create ()
                  if (element == obj.id)
                  {
                      obj.status = "alreadyBuilt";
+                     window[obj.name].setFrame(1);
                  }
             }); 
             console.log(element);
@@ -68,19 +94,33 @@ function create ()
 
         DataObject.On(key, function(newValue){
             txt.setText(key+' : '+newValue);
+            if(key==="Crystals"){
+                //ICI update le text
+            }
         })
     }
-
-    setTimeout(() => {
-        PlayerData.Currency.Gold +=1; //set value
-        DataObject.updateData(); //update
-    }, 2000);
 }
-    
+
+function update(){
+    if(Date.now() - PlayerData.lastAward > 1000){
+        PlayerData.lastAward = Date.now();
+        let gold = 0;
+
+        console.log(PlayerData.UnlockedBuildings);
+        for (const [key, value] of Object.entries(PlayerData.UnlockedBuildings)) {
+            gold += attractions[value-1].rendings;
+        }
+        
+        PlayerData.Currency.Gold += gold;
+
+        DataObject.updateData();
+    }
+}
 
 
 function checkBuilding(building)
-{   
+{  
+    selectedBuilding = building.id;
     switch (building.status){
         case "build":
             displayBuildWindow(building);
@@ -90,14 +130,13 @@ function checkBuilding(building)
             break;
     }
 }
-function construire(building,popup,argent){
+function construire(building,popup){
 
     let prix = building.price;
     if (PlayerData.Currency.Gold >= prix)
     {
-        //console.log(PlayerData.Currency.Gold);
         PlayerData.Currency.Gold -= prix;
-        //console.log(PlayerData.Currency.Gold);
+        window[building.name].setFrame(1);
         building.status = "alreadyBuilt";
         PlayerData.UnlockedBuildings.push(building.id)
         DataObject.updateData();
@@ -109,21 +148,37 @@ function construire(building,popup,argent){
         popup.style.display = 'none';
         //window.alert("il vous manque " + (prix - argent) + " or")
     }
+    enableAll();
 
 }
 function displayBuildWindow (building)
 {
     
+    disableAll();
+
+    let build = document.getElementById("build-button");
+    
+    
+    build.addEventListener("click", () => construire(building,popup))
+
     let popup = document.getElementById('construction');
     if (popup && popup.style.display === 'none' ){
         popup.style.display = 'block';
     }
+    document.getElementById("price").innerHTML = "Price : " + building.price + " Gold"
+    document.getElementById("name").innerHTML = building.name
+    document.getElementById("rendings").innerHTML = "Rendings : " + building.rendings + " Gold per second"
+
+    
+   
+
     let button = document.getElementById("close");
-    button.addEventListener("click", () => (
+    button.addEventListener("click", () => {
         popup.style.display = 'none'
-    ))
-    let build = document.getElementById("build-button");
-    build.addEventListener("click", () => construire(building,popup))
+        build.removeEventListener("click", () => construire(building,popup))
+        enableAll();
+    })
+    
 
 }
 
