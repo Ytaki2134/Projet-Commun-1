7841779b4eb7b5fe99ce29f506b51c586447f91d
TOM LE_GROS <freezekaz.dement@gmail.com>
2022-04-04 09:44:21 +0200

    Building done + linked with local storage

---
+++ Projet-commun-1/src/main.js
@@ -0,0 +1,133 @@
+import Data from "./utilities/dataHandling.js";
+import * as apiUtilities from "./utilities/apiUtilities.js";
+
+const attractions = await apiUtilities.getAttractions();
+
+let DataObject = new Data();
+let PlayerData = DataObject.getData();
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    //this.load.setBaseURL('http://labs .phaser.io');
+    this.load.image('background', 'image/background.png');
+    this.load.image('field', 'image/field.png');
+    this.load.image('built', 'image/built.png');
+}
+
+function create ()
+{   
+    //alert(myBuildings[0].name);
+    //console.log(attractions[2].name);
+    var argent = 1500; // simulation de l'argent de l'utilisateur
+    this.add.image(900, 485, 'background'); 
+
+    attractions.forEach(obj => {
+        console.log(obj.name);
+        window[obj.name] = this.add.image(    
+            obj.pos_x,
+            obj.pos_y,
+            obj.image);
+        window[obj.name].setInteractive().on('pointerdown', () => checkBuilding(obj,argent));
+
+    });
+    let pos = 0;
+    for (const [key, value] of Object.entries(PlayerData.Currency)) {
+        let txt = this.add.text(100*pos, 0, key+' : '+value, { font: '"Press Start 2P"' });
+        pos += 1;
+
+        DataObject.On(key, function(newValue){
+            txt.setText(key+' : '+newValue);
+        })
+    }
+
+    setTimeout(() => {
+        PlayerData.Currency.Gold +=1; //set value
+        DataObject.updateData(); //update
+    }, 2000);
+}
+    
+
+
+function checkBuilding(building)
+{   
+    switch (building.status){
+        case "build":
+            displayBuildWindow(building);
+            break;
+        case "alreadyBuilt":
+            displayUpgradeWindow(building)
+            break;
+    }
+}
+function construire(building,popup,argent){
+
+    let prix = building.price;
+    if (PlayerData.Currency.Gold >= prix)
+    {
+        console.log(PlayerData.Currency.Gold);
+        PlayerData.Currency.Gold -= prix;
+        console.log(PlayerData.Currency.Gold);
+        building.status = "alreadyBuilt";
+        PlayerData.UnlockedBuildings.push(building.id)
+        DataObject.updateData();
+        popup.style.display = 'none';
+        
+    }
+    else
+    {
+        popup.style.display = 'none';
+        //window.alert("il vous manque " + (prix - argent) + " or")
+    }
+
+}
+function displayBuildWindow (building)
+{
+    
+    let popup = document.getElementById('construction');
+    if (popup && popup.style.display === 'none' ){
+        popup.style.display = 'block';
+    }
+    let button = document.getElementById("close");
+    button.addEventListener("click", () => (
+        popup.style.display = 'none'
+    ))
+    let build = document.getElementById("build-button");
+    build.addEventListener("click", () => construire(building,popup))
+
+}
+
+
+function displayUpgradeWindow (building)
+{
+    
+    let popup = document.getElementById('upgrade');
+    if (popup && popup.style.display === 'none' ){
+        popup.style.display = 'block';
+    }
+    let button = document.getElementById("close-upgrade");
+    button.addEventListener("click", () => (
+        popup.style.display = 'none'    
+    ))
+    let build = document.getElementById("upgrade-button");
+    build.addEventListener("click", () => construire(building,popup,argent))
+
+}
---
+++ Projet-commun-1/src/utilities/dataHandling.js
@@ -0,0 +1,67 @@
+const baseData = {
+    Currency: {
+        Gold: 1600,
+        Crystals: 0,
+    },
+    UnlockedBuildings: [
+
+    ]
+}
+
+class Data{
+    constructor(){
+        let playerData = localStorage.getItem('data');
+        if(!playerData || playerData == null){
+            localStorage.setItem('data', JSON.stringify(baseData));
+            playerData = localStorage.getItem('data');
+        }else{
+            console.log("?");
+            playerData = this.fixData(JSON.parse(playerData), baseData);
+            localStorage.setItem('data', JSON.stringify(playerData));
+        }
+        this.playerData = {...playerData};
+        this.savedPlayerData = JSON.parse(JSON.stringify(this.playerData)); // DEEP CLONE
+        this.listenValueChanged = {};
+    }
+
+    fixData(playerData, base){
+        for (const [key, value] of Object.entries(base)) {
+            if(playerData[key] === undefined){
+                playerData[key] = value
+                console.log(key);
+            } else if(typeof(value)=="object" && value != null){
+                console.log(key);
+                playerData[key] = this.fixData(playerData[key], value);
+            }
+        }
+        return playerData 
+    }
+
+    getData(){
+        return this.playerData;
+    }
+
+    On(key, fct){
+        this.listenValueChanged[key] = fct;
+    }
+
+    onChangeValue(original, clone){ //clone should be on an hight version than original
+        for (const [key, value] of Object.entries(clone)) {
+            if(typeof(value)==="object" && value != null){
+                this.onChangeValue(original[key], clone[key]);
+            }else if(original[key] != undefined && original[key] != value){
+                if(this.listenValueChanged[key]){
+                    this.listenValueChanged[key](value);
+                }
+            }
+        }
+    }
+
+    updateData(){
+        this.onChangeValue(this.savedPlayerData, this.playerData);
+        this.savedPlayerData = JSON.parse(JSON.stringify(this.playerData));;
+        localStorage.setItem('data', JSON.stringify(this.savedPlayerData));
+    }
+}
+
+export default Data;
